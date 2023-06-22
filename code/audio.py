fd2549d5f4042ab26b962b611b5105a8f1fa28f3
gaddyh <gaddyh@gmail.com>
2023-06-22 03:07:01 +0300

    Add files via upload

--- code/audio.py
+++ code/audio.py
@@ -9,46 +9,56 @@ from scipy import signal
 
 from audio2numpy import open_audio
 
-def load(path):
-	signal, sampling_rate = open_audio(path)
-	signal = np.pad(signal, (0, max(0, sample_rate - len(signal))), "constant")
-	return signal
-	
-def load_tensor(path, sample_rate):
-  x = tf.io.read_file(str(path))
-  waveform, _ = tf.audio.decode_wav(x, desired_channels=1, desired_samples=sample_rate,)
-  return waveform
-  
-import math
-def load_max_samples(path):
-  samples = []
 
-  signal, sampling_rate = open_audio(path)
-  if(sampling_rate == sample_rate):
-    if len(signal) < sample_rate :
-      signal = np.pad(signal, (0, max(0, sample_rate - len(signal))), "constant")
-      samples.append(signal)
-    else:
-      count = math.floor(len(signal)/sample_rate)
-      for i in range(count):
-        if i == 0:
-          samples.append( signal[:sample_rate])
+def load(path):
+    signal, sampling_rate = open_audio(path)
+    signal = np.pad(signal, (0, max(0, sample_rate - len(signal))), "constant")
+    return signal
+
+
+def load_tensor(path, sample_rate):
+    x = tf.io.read_file(str(path))
+    waveform, _ = tf.audio.decode_wav(
+        x,
+        desired_channels=1,
+        desired_samples=sample_rate,
+    )
+    return waveform
+
+
+import math
+
+
+def load_max_samples(path):
+    samples = []
+
+    signal, sampling_rate = open_audio(path)
+    if sampling_rate == sample_rate:
+        if len(signal) < sample_rate:
+            signal = np.pad(signal, (0, max(0, sample_rate - len(signal))), "constant")
+            samples.append(signal)
         else:
-          start = sample_rate * i
-          end = sample_rate * (i+1)
-          samples.append( signal[start:end])
-  else:
-    print('bad sample rate for ' , path, 'sample is: ', sampling_rate)
-  
-  return samples
-  
+            count = math.floor(len(signal) / sample_rate)
+            for i in range(count):
+                if i == 0:
+                    samples.append(signal[:sample_rate])
+                else:
+                    start = sample_rate * i
+                    end = sample_rate * (i + 1)
+                    samples.append(signal[start:end])
+    else:
+        print("bad sample rate for ", path, "sample is: ", sampling_rate)
+
+    return samples
+
 
 def resample_example():
-  x = np.linspace(0, 10, 20, endpoint=False)
-  y = np.cos(-x**2/6.0)
-  f = signal.resample(y, 100)
-  xnew = np.linspace(0, 10, 100, endpoint=False)
-  import matplotlib.pyplot as plt
-  plt.plot(x, y, 'go-', xnew, f, '.-', 10, y[0], 'ro')
-  plt.legend(['data', 'resampled'], loc='best')
-  plt.show()
+    x = np.linspace(0, 10, 20, endpoint=False)
+    y = np.cos(-(x ** 2) / 6.0)
+    f = signal.resample(y, 100)
+    xnew = np.linspace(0, 10, 100, endpoint=False)
+    import matplotlib.pyplot as plt
+
+    plt.plot(x, y, "go-", xnew, f, ".-", 10, y[0], "ro")
+    plt.legend(["data", "resampled"], loc="best")
+    plt.show()
--- code/prepare_pairs.py
+++ code/prepare_pairs.py
@@ -18,148 +18,148 @@ TEST_PATH = 'WhistleNet/media/test/'
 MEDIA_PATH = 'WhistleNet/media/'
 
 def getall(media_dir):
-  samplesbycategory = []
-  i=0
-  for path in os.listdir(media_dir):
-    samples_dir = os.path.join(media_dir, path)
-    samplesbycategory[i] = get_set(samples_dir)
-    i=i+1
-  return samplesbycategory
+	samplesbycategory = []
+	i=0
+	for path in os.listdir(media_dir):
+		samples_dir = os.path.join(media_dir, path)
+		samplesbycategory[i] = get_set(samples_dir)
+		i=i+1
+	return samplesbycategory
 
-  
+	
 def get_set(path):
 	samples = get_samples(path)
 	samples = augment_spec(samples)
 	return samples
-  
+	
 def get_samples(path):
 	samples=[]
 	for file in os.listdir(path):
-	    #print(file)
-	    fp = os.path.join(path, file)
-      samples=samples+load_max_samples(fp)
+			#print(file)
+			fp = os.path.join(path, file)
+			samples=samples+load_max_samples(fp)
 	return samples
 
 def augment_spec(samples):
 	signals=[]
 	for sample in samples:
-	  signal = np.array(sample)
-	  signal = np.squeeze(signal)
-	  #print(signal.shape,4)
-	  signals.append(signal)
-	  signals.append(noise(signal, noise_factor = 0.005 ))
-	  signals.append(stretch(signal, 1.2))
-	  signals.append(stretch(np.array(signal), 0.78))
-	  signals.append(np.roll(signal, 300))
+		signal = np.array(sample)
+		signal = np.squeeze(signal)
+		#print(signal.shape,4)
+		signals.append(signal)
+		signals.append(noise(signal, noise_factor = 0.005 ))
+		signals.append(stretch(signal, 1.2))
+		signals.append(stretch(np.array(signal), 0.78))
+		signals.append(np.roll(signal, 300))
 
 	signals = list(map(get_spectrogram, signals))
 
 	return signals
 
 def split(samples_same_category, num_test):
-  test = samples_same_category[:num_test]
-  train = samples_same_category[num_test:]
-  return train, test
-  
+	test = samples_same_category[:num_test]
+	train = samples_same_category[num_test:]
+	return train, test
+	
 def prnt(str):
-  print("\n".join(str))
-  
+	print("\n".join(str))
+	
 def create_ds_pairs(anchors1, negatives1):
-  #when anchors only on left val[:,0] is bad val[:,1]] is excelent
-  l = len(anchors1)
-  lh = math.floor(l/2)
+	#when anchors only on left val[:,0] is bad val[:,1]] is excelent
+	l = len(anchors1)
+	lh = math.floor(l/2)
 
-  negatives = list(itertools.product(anchors1[:lh], negatives1))
-  negatives = negatives + list(itertools.product(negatives1, anchors1[lh:]))
-  anchors = list(itertools.combinations(anchors1,2))
+	negatives = list(itertools.product(anchors1[:lh], negatives1))
+	negatives = negatives + list(itertools.product(negatives1, anchors1[lh:]))
+	anchors = list(itertools.combinations(anchors1,2))
 
-  X = anchors + negatives
-  labels = np.ones(len(anchors), dtype=int).tolist() + np.zeros((len(negatives),), dtype=int).tolist()
+	X = anchors + negatives
+	labels = np.ones(len(anchors), dtype=int).tolist() + np.zeros((len(negatives),), dtype=int).tolist()
 
-  print(len(labels))
+	print(len(labels))
 
-  X = np.array(X)
-  labels = np.array(labels)
+	X = np.array(X)
+	labels = np.array(labels)
 
-  p = [x for x in range(len(labels))]
-  random.shuffle(p)
+	p = [x for x in range(len(labels))]
+	random.shuffle(p)
 
-  X = X[p]
-  labels = labels[p]
+	X = X[p]
+	labels = labels[p]
 
-  return X, labels
-  
+	return X, labels
+	
 def create_anchors_ds_pairs(anchors1, anchors2):
-  #when anchors only on left val[:,0] is bad val[:,1]] is excelent
-  l = len(anchors1)
-  lh = math.floor(l/2)
+	#when anchors only on left val[:,0] is bad val[:,1]] is excelent
+	l = len(anchors1)
+	lh = math.floor(l/2)
 
-  negatives = list(itertools.product(anchors1[:lh], anchors2))
-  negatives = negatives + list(itertools.product(anchors2, anchors1[lh:]))
-  
-  anchors = list(itertools.combinations(anchors1,2))
-  anchors = anchors + list(itertools.combinations(anchors2,2))
+	negatives = list(itertools.product(anchors1[:lh], anchors2))
+	negatives = negatives + list(itertools.product(anchors2, anchors1[lh:]))
+	
+	anchors = list(itertools.combinations(anchors1,2))
+	anchors = anchors + list(itertools.combinations(anchors2,2))
 
-  X = anchors + negatives
-  labels = np.ones(len(anchors), dtype=int).tolist() + np.zeros((len(negatives),), dtype=int).tolist()
+	X = anchors + negatives
+	labels = np.ones(len(anchors), dtype=int).tolist() + np.zeros((len(negatives),), dtype=int).tolist()
 
-  print(len(labels))
+	print(len(labels))
 
-  X = np.array(X)
-  labels = np.array(labels)
+	X = np.array(X)
+	labels = np.array(labels)
 
-  p = [x for x in range(len(labels))]
-  random.shuffle(p)
+	p = [x for x in range(len(labels))]
+	random.shuffle(p)
 
-  X = X[p]
-  labels = labels[p]
+	X = X[p]
+	labels = labels[p]
 
-  return X, labels
+	return X, labels
 
 def create_anchors_list_pairs(anchors1, anchors2, anchors3):
-  #when anchors only on left val[:,0] is bad val[:,1]] is excelent
-  l = len(anchors1)
-  lh = math.floor(l/2)
+	#when anchors only on left val[:,0] is bad val[:,1]] is excelent
+	l = len(anchors1)
+	lh = math.floor(l/2)
 
-  l2 = len(anchors2)
-  lh2 = math.floor(l2/2)
-  
-  negatives = list(itertools.product(anchors1[:lh], anchors2)) + list(itertools.product(anchors2, anchors1[lh:])) + list(itertools.product(anchors1[:lh], anchors3)) + list(itertools.product(anchors3, anchors1[lh:])) + list(itertools.product(anchors2[:lh2], anchors3)) + list(itertools.product(anchors3, anchors2[lh2:]))
-              
-  
-  anchors = list(itertools.combinations(anchors1,2)) + list(itertools.combinations(anchors2,2)) + list(itertools.combinations(anchors3,2))
+	l2 = len(anchors2)
+	lh2 = math.floor(l2/2)
+	
+	negatives = list(itertools.product(anchors1[:lh], anchors2)) + list(itertools.product(anchors2, anchors1[lh:])) + list(itertools.product(anchors1[:lh], anchors3)) + list(itertools.product(anchors3, anchors1[lh:])) + list(itertools.product(anchors2[:lh2], anchors3)) + list(itertools.product(anchors3, anchors2[lh2:]))
+							
+	
+	anchors = list(itertools.combinations(anchors1,2)) + list(itertools.combinations(anchors2,2)) + list(itertools.combinations(anchors3,2))
 
-  X = anchors + negatives
-  labels = np.ones(len(anchors), dtype=int).tolist() + np.zeros((len(negatives),), dtype=int).tolist()
-  
-  return X, labels
+	X = anchors + negatives
+	labels = np.ones(len(anchors), dtype=int).tolist() + np.zeros((len(negatives),), dtype=int).tolist()
+	
+	return X, labels
 
 
 def create_anchors_ds_pairs(anchors1, anchors2, anchors3):
-  #when anchors only on left val[:,0] is bad val[:,1]] is excelent
-  l = len(anchors1)
-  lh = math.floor(l/2)
+	#when anchors only on left val[:,0] is bad val[:,1]] is excelent
+	l = len(anchors1)
+	lh = math.floor(l/2)
 
-  l2 = len(anchors2)
-  lh2 = math.floor(l2/2)
-  
-  negatives = list(itertools.product(anchors1[:lh], anchors2)) + list(itertools.product(anchors2, anchors1[lh:])) + list(itertools.product(anchors1[:lh], anchors3)) + list(itertools.product(anchors3, anchors1[lh:])) + list(itertools.product(anchors2[:lh2], anchors3)) + list(itertools.product(anchors3, anchors2[lh2:]))
-              
-  
-  anchors = list(itertools.combinations(anchors1,2)) + list(itertools.combinations(anchors2,2)) + list(itertools.combinations(anchors3,2))
+	l2 = len(anchors2)
+	lh2 = math.floor(l2/2)
+	
+	negatives = list(itertools.product(anchors1[:lh], anchors2)) + list(itertools.product(anchors2, anchors1[lh:])) + list(itertools.product(anchors1[:lh], anchors3)) + list(itertools.product(anchors3, anchors1[lh:])) + list(itertools.product(anchors2[:lh2], anchors3)) + list(itertools.product(anchors3, anchors2[lh2:]))
+							
+	
+	anchors = list(itertools.combinations(anchors1,2)) + list(itertools.combinations(anchors2,2)) + list(itertools.combinations(anchors3,2))
 
-  X = anchors + negatives
-  labels = np.ones(len(anchors), dtype=int).tolist() + np.zeros((len(negatives),), dtype=int).tolist()
+	X = anchors + negatives
+	labels = np.ones(len(anchors), dtype=int).tolist() + np.zeros((len(negatives),), dtype=int).tolist()
 
-  print(len(labels))
+	print(len(labels))
 
-  X = np.array(X)
-  labels = np.array(labels)
+	X = np.array(X)
+	labels = np.array(labels)
 
-  p = [x for x in range(len(labels))]
-  random.shuffle(p)
+	p = [x for x in range(len(labels))]
+	random.shuffle(p)
 
-  X = X[p]
-  labels = labels[p]
+	X = X[p]
+	labels = labels[p]
 
-  return X, labels
+	return X, labels
