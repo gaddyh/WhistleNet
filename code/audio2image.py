a8802ba9fdb01b408ddb6f13395798899ef2fd0a
gaddyh <gaddyh@gmail.com>
2023-06-24 03:01:04 +0300

    Add files via upload

--- code/audio2image.py
+++ code/audio2image.py
@@ -5,10 +5,50 @@ def get_spectrogram(waveform):
   spectrogram = tf.signal.stft(
       waveform, frame_length=255, frame_step=128)
   # Obtain the magnitude of the STFT.
-  spectrogram = tf.abs(spectrogram)
+  spectrograms = tf.abs(spectrogram)
   # Add a `channels` dimension, so that the spectrogram can be used
   # as image-like input data with convolution layers (which expect
   # shape (`batch_size`, `height`, `width`, `channels`).
-  spectrogram = spectrogram[..., tf.newaxis]
-  return spectrogram
+  #spectrogram = spectrogram[..., tf.newaxis]
+  num_spectrogram_bins = stfts.shape[-1].value
+  lower_edge_hertz, upper_edge_hertz, num_mel_bins = 80.0, 7600.0, 80
+  linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
+  num_mel_bins, num_spectrogram_bins, sample_rate, lower_edge_hertz,
+  upper_edge_hertz)
+  mel_spectrograms = tf.tensordot(
+    spectrograms, linear_to_mel_weight_matrix, 1)
+  mel_spectrograms.set_shape(spectrograms.shape[:-1].concatenate(
+    linear_to_mel_weight_matrix.shape[-1:]))
+
+# Compute a stabilized log to get log-magnitude mel-scale spectrograms.
+  log_mel_spectrograms = tf.math.log(mel_spectrograms + 1e-6)
+
+# Compute MFCCs from log_mel_spectrograms and take the first 13.
+  mfccs = tf.signal.mfccs_from_log_mel_spectrograms(
+    log_mel_spectrograms)[..., :13]
+  return mfccs
+  
+  
+  
+  stfts = tf.signal.stft(pcm, frame_length=1024, frame_step=256,
+                       fft_length=1024)
+spectrograms = tf.abs(stfts)
+
+# Warp the linear scale spectrograms into the mel-scale.
+num_spectrogram_bins = stfts.shape[-1].value
+lower_edge_hertz, upper_edge_hertz, num_mel_bins = 80.0, 7600.0, 80
+linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
+  num_mel_bins, num_spectrogram_bins, sample_rate, lower_edge_hertz,
+  upper_edge_hertz)
+mel_spectrograms = tf.tensordot(
+  spectrograms, linear_to_mel_weight_matrix, 1)
+mel_spectrograms.set_shape(spectrograms.shape[:-1].concatenate(
+  linear_to_mel_weight_matrix.shape[-1:]))
+
+# Compute a stabilized log to get log-magnitude mel-scale spectrograms.
+log_mel_spectrograms = tf.math.log(mel_spectrograms + 1e-6)
+
+# Compute MFCCs from log_mel_spectrograms and take the first 13.
+mfccs = tf.signal.mfccs_from_log_mel_spectrograms(
+  log_mel_spectrograms)[..., :13]
   
